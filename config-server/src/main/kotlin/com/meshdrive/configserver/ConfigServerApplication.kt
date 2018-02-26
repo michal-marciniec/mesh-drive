7e0a42cc660d5f817ae06a9afaa814db09260ef3
Michal Marciniec <michal.marciniec@gmail.com>
2018-02-26 16:33:02 +0100

    discovery-service base

--- config-server/src/main/kotlin/com/meshdrive/configserver/ConfigServerApplication.kt
+++ config-server/src/main/kotlin/com/meshdrive/configserver/ConfigServerApplication.kt
@@ -3,9 +3,11 @@ package com.meshdrive.configserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 class ConfigServerApplication
 
 fun main(args: Array<String>) {
--- hello-service/src/main/kotlin/com/meshdrive/helloservice/HelloServiceApplication.kt
+++ hello-service/src/main/kotlin/com/meshdrive/helloservice/HelloServiceApplication.kt
@@ -2,8 +2,10 @@ package com.meshdrive.helloservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class HelloServiceApplication
 
 fun main(args: Array<String>) {
